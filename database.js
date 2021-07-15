cc8dcf34c0256e1f2bd1af26d49471ccff65acd7
Caio Matheus <62734622+CaioMatheu5@users.noreply.github.com>
2021-07-15 19:24:12 -0300

    v1.0

---
+++ database.js
@@ -0,0 +1,40 @@
+const livros = [
+    {
+        id: 1,
+        nome:"Zeus",
+        autor:"Zeca Pagodinho",
+        categoria:"Aventura",
+        paginas: 213,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 2,
+        nome:"Aló, estou aqui",
+        autor:"Ricky",
+        categoria:"Suspense",
+        paginas: 412,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 3,
+        nome:"Avó e avô",
+        autor:"Eliane Gusmão",
+        categoria:"História",
+        paginas: 146,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 4,
+        nome:"Titã Romeu",
+        autor:"Charles Junior",
+        categoria:"Guerra",
+        paginas: 431,
+        recomenda: false,
+        leu: false
+    }
+]
+
+module.exports = livros
